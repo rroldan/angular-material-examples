87a80ec45b397f7fdf8a9f21a08e3451e98b0c97
rroldan <roldan.ricardo@gmail.com>
2020-04-10 18:32:48 +0200

    Add components

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -12,8 +12,8 @@ export class SidenavComponent implements OnInit {
   private mediaMatcher: MediaQueryList =
     matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
 
-  constructor() {
-
+  constructor(zone: NgZone) {
+    this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`)));
   }
 
   ngOnInit() {
